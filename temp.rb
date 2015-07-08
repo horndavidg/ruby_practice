b5d749d52e6b425ceadc524e868f973df09b1e3e
David Horn <horndavidg@gmail.com>
2015-07-08 08:40:44 -0700

    update temp file

--- temp.rb
+++ temp.rb
@@ -8,14 +8,14 @@ if type == 1
 puts "Enter Celsius Temperature"
 temp = gets.chomp.to_i
 
-puts temp * 9/5 + 32
+puts "That is #{temp * 9/5 + 32} Fahrenheit" 
 
 else
 
 puts "Enter Fahrenheit Temperature"
 temp = gets.chomp.to_i
 
-puts (temp-32) * 5/9
+puts "That is #{(temp-32) * 5/9} Celsius"
 
 end
 
